f6c74019ee3bc0d6bd5231e73d5613621ac1e2d0
Asmitha M <144476066+asmitha03@users.noreply.github.com>
2025-05-27 16:02:26 +0530

    first commit

---
+++ python/Control Statements.py
@@ -0,0 +1,62 @@
+## Control Statements(Conditional Statements)
+
+#if statements
+a=10
+if a > 5 :
+    print("It is true")
+
+#if-else
+b=10
+if b < 5 :
+    print("it is true")
+else :
+    print("it is false")
+
+#if-elif-else statements
+c=10
+if c == 6 :
+    print("it is equal")
+elif c == 8 :
+    print("it is not equal")
+else :
+    print("both are not equal")
+
+    #looping Statements
+
+#for loop (integer)
+for b in range(0,9):
+  print (b)
+#string
+for a in ('asmitha'):
+    print(a)
+
+#while loop
+    a= 5
+    while a <=10:
+        print(a)
+        a +=1
+
+#switched Statements
+        #continue
+b = 0
+while b < 5:
+    b += 1
+    if  b == 3:
+        continue
+        print(b)
+
+#break
+        b = 0
+while b < 5:
+    b += 1
+    if  b == 3:
+        break
+        print(b)
+
+    
+
+        
+        
+
+
+    
